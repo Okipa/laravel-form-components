3742d8c736d0bf5b23ab3a4bb7f88aa1a9c8e6a8
Quentin <quentin@acid.fr>
2022-10-11 13:51:35 +0200

    Fix bs4 checkbox & radio classes

--- resources/views/bootstrap-4/radio.blade.php
+++ resources/views/bootstrap-4/radio.blade.php
@@ -13,17 +13,17 @@
             $radioId = $getId(suffix: $groupValue) ?: $getDefaultId(prefix: 'radio', suffix: $groupValue);
             $checked = $getGroupModeCheckedStatus($groupValue);
         @endphp
-        <div @class(['form-check', 'form-check-inline' => $inline])>
+        <div @class(['custom-control', 'custom-radio', 'custom-control-inline' => $inline])>
             <input {{ $attributes->merge([
                 'wire:model' . $getComponentLivewireModifier() => $isWired && ! $hasComponentNativeLivewireModelBinding() ? $name : null,
                 'id' => $radioId,
-                'class' => 'form-check-input',
+                'class' => 'custom-control-input',
                 'name' => $name,
                 'value' => $groupValue,
                 'checked' => $isWired ? null : $checked,
                 'aria-describedby' => $caption ? $captionId . '-caption' : null,
             ]) }} type="radio">
-            <x:form::partials.label :id="$radioId" class="form-check-label" :label="$groupLabel"/>
+            <x:form::partials.label :id="$radioId" class="custom-control-label" :label="$groupLabel"/>
         </div>
     @endforeach
     <x:form::partials.caption :inputId="$captionId" :caption="$caption"/>
